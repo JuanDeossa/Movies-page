2fe5c15f28806e6bc83ff2704ed0adf799c3194d
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-02 15:33:19 -0500

    categories preview

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 const API_KEY="804aa1781acab394d667d99070ae4b0f"
 // https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
+// https://api.themoviedb.org/3/genre/movie/list?api_key=<<api_key>>
 
 const trendingMoviesPreview = document.querySelector("#trendingPreview > article") 
+const categoriesPreview = document.querySelector("#categoriesPreview > article") 
 
 async function getTrendingMovies() {
     const res = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
     try {
         let data = await res.json()
         data = data.results
-        console.log(data);
+        // console.log(data);
         data.forEach((element,index) => {
             const altName = element.title ?? element.name
             trendingMoviesPreview.innerHTML+=`
@@ -24,8 +26,27 @@ async function getTrendingMovies() {
 
 getTrendingMovies() 
 
+async function getCategoriesMovies() {
+    const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
+    try {
+        let data = await res.json()
+        data = data.genres
+        // console.log(data);
+        data.forEach((element,index) => {
+            categoriesPreview.innerHTML+=`
+            <div class="category-container">
+                <h3 id="id${element.id}" class="category-title">${element.name}</h3>
+            </div>
+            `
+        });
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+getCategoriesMovies() 
+
+
 /*
-<div class="movie-container">
-<imgsrc="https://image.tmdb.org/t/p/w300/adOzdWS35KAo21r9R4BuFCkLer6.jpg"class="movie-img"alt="Nombre de la película"/>
-</div>
+
  */
