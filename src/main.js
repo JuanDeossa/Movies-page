45909c2ad7cba0111674b5413b601511c4bdc4c9
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-02 14:55:49 -0500

    Trending movies preview OK

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const API_KEY="804aa1781acab394d667d99070ae4b0f"
 // https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>
 
+const trendingMoviesPreview = document.querySelector("#trendingPreview > article") 
+
 async function getTrendingMovies() {
     const res = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`)
     try {
@@ -8,13 +10,12 @@ async function getTrendingMovies() {
         data = data.results
         console.log(data);
         data.forEach((element,index) => {
-            if (element.hasOwnProperty("title")) {
-                console.log(`${index+1} ${element.title}`);
-            }else if (element.hasOwnProperty("name")) {
-                console.log(`${index+1} ${element.name}`);
-            }else{
-                console.log("No name and no title");
-            }
+            const altName = element.title ?? element.name
+            trendingMoviesPreview.innerHTML+=`
+            <div class="movie-container">
+                <img src="https://image.tmdb.org/t/p/w300/${element.poster_path} "class="movie-img"alt="${altName}"/>
+            </div>
+            `
         });
     } catch (error) {
         throw new Error(error)
@@ -22,3 +23,9 @@ async function getTrendingMovies() {
 }
 
 getTrendingMovies() 
+
+/*
+<div class="movie-container">
+<imgsrc="https://image.tmdb.org/t/p/w300/adOzdWS35KAo21r9R4BuFCkLer6.jpg"class="movie-img"alt="Nombre de la película"/>
+</div>
+ */
